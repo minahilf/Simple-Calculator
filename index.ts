3a41214021584d31659f32283089fb48dcaf138a
minahilfatima <sminahilfatima@gmail.com>
2024-03-26 09:24:34 +0500

    added modulus and exponentiation functions

--- index.ts
+++ index.ts
@@ -8,7 +8,7 @@ const answer = await inquirer.prompt([
     message: "Choose operator:",
     type: "list",
     name: "operator",
-    choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    choices: ["Addition", "Subtraction", "Multiplication", "Division", "Modulus", "Exponentiation"],
   },
 ]);
 
@@ -20,6 +20,10 @@ if (answer.operator === "Addition") {
   console.log(`Your value is: ${answer.firstNumber * answer.secondNumber}`);
 } else if (answer.operator === "Division") {
   console.log(`Your value is: ${answer.firstNumber / answer.secondNumber}`);
+} else if (answer.operator === "Modulus"){
+  console.log(`Your value is: ${answer.firstNumber % answer.secondNumber}`)
+} else if(answer.operator === "Exponentiation"){
+  console.log(`Your value is ${answer.firstNumber**answer.secondNumber}`)
 } else {
   console.log(`Please select valid operator`);
 }
